bf199d5f0721a4342a755cdcc1c4d98b1e712c7f
Abdulrahman Dawoud <kabdo2223@gmail.com>
2025-10-20 20:51:12 +0300

    configure supabase

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,16 @@ import 'package:e_commerce_dashboard/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://skzbelzesdrnxhsthsat.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNremJlbHplc2Rybnhoc3Roc2F0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA5NjE1NzQsImV4cCI6MjA3NjUzNzU3NH0.U5hDukmqmVgITL1_Z6DP6xxSnjVKRqBBnh-QlmfzAhI',
+  );
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   setupGetIt();
   Bloc.observer = CustomBlocObserver();

6d7f38307fd2c71d1d9b2ba147c8d0640a424ab1
Abdulrahman Dawoud <kabdo2223@gmail.com>
2025-10-22 14:30:22 +0300

    create bucket and upload file to supabase

--- lib/core/services/supabase_storage_service.dart
+++ lib/core/services/supabase_storage_service.dart
@@ -7,6 +7,10 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseStorageService implements StorageService {
   static late Supabase _supabase;
+  static createBuckets(String bucketName) async {
+    await _supabase.client.storage.createBucket(bucketName);
+  }
+
   static initSupabase() async {
     _supabase = await Supabase.initialize(
       url: kSupabaseUrl,
--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SupabaseStorageService.initSupabase();
+  await SupabaseStorageService.createBuckets('products_images');
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   setupGetIt();
   Bloc.observer = CustomBlocObserver();

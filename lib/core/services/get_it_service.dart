88168745ce3009fbb41366ce87ec3dc44fb0e0d3
Abdulrahman Dawoud <kabdo2223@gmail.com>
2025-10-22 14:00:33 +0300

    impl upload file with supabase

--- lib/core/services/get_it_service.dart
+++ lib/core/services/get_it_service.dart
@@ -3,15 +3,15 @@ import 'package:e_commerce_dashboard/core/repos/images_repo/images_repo_impl.dar
 import 'package:e_commerce_dashboard/core/repos/products_repo/product_repo.dart';
 import 'package:e_commerce_dashboard/core/repos/products_repo/product_repo_impl.dart';
 import 'package:e_commerce_dashboard/core/services/data_service.dart';
-import 'package:e_commerce_dashboard/core/services/firebase_storage_service.dart';
 import 'package:e_commerce_dashboard/core/services/firestore_services.dart';
 import 'package:e_commerce_dashboard/core/services/storage_service.dart';
+import 'package:e_commerce_dashboard/core/services/supabase_storage_service.dart';
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
 
 void setupGetIt() {
-  getIt.registerSingleton<StorageService>(FirebaseStorageService());
+  getIt.registerSingleton<StorageService>(SupabaseStorageService());
   getIt.registerSingleton<DatabaseServices>(FirestoreServices());
 
   getIt.registerSingleton<ImagesRepo>(
--- lib/core/services/supabase_storage_service.dart
+++ lib/core/services/supabase_storage_service.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:e_commerce_dashboard/core/services/storage_service.dart';
 import 'package:e_commerce_dashboard/core/utils/keys.dart';
+import 'package:path/path.dart' as b;
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseStorageService implements StorageService {
@@ -14,8 +15,12 @@ class SupabaseStorageService implements StorageService {
   }
 
   @override
-  Future<String> uploadFile(File file, String path) {
-    // TODO: implement uploadFile
-    throw UnimplementedError();
+  Future<String> uploadFile(File file, String path) async {
+    String fileName = b.basename(file.path);
+    String extensionName = b.extension(file.path);
+    var result = await _supabase.client.storage
+        .from('products_images')
+        .upload('$path/$fileName.$extensionName', file);
+    return result;
   }
 }

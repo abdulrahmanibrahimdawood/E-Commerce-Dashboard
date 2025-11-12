622702a2d0d01b5ed2761c54444fd4833b6f7aec
Abdulrahman Dawoud <kabdo2223@gmail.com>
2025-11-12 14:32:15 +0200

    create orders view

--- lib/core/helper_funcation/on_generate_route.dart
+++ lib/core/helper_funcation/on_generate_route.dart
@@ -1,5 +1,6 @@
 import 'package:e_commerce_dashboard/features/add_product/presentation/views/add_product_view.dart';
 import 'package:e_commerce_dashboard/features/dashboard/presentation/views/dashboaed_view.dart';
+import 'package:e_commerce_dashboard/features/orders/presentation/views/orders_view.dart';
 import 'package:flutter/material.dart';
 
 Route<dynamic> onGenerateRoutes(RouteSettings settings) {
@@ -8,6 +9,8 @@ Route<dynamic> onGenerateRoutes(RouteSettings settings) {
       return MaterialPageRoute(builder: (context) => const DashboaedView());
     case AddProductView.routeName:
       return MaterialPageRoute(builder: (context) => const AddProductView());
+    case OrdersView.routeName:
+      return MaterialPageRoute(builder: (context) => const OrdersView());
     default:
       return MaterialPageRoute(builder: (context) => const Scaffold());
   }

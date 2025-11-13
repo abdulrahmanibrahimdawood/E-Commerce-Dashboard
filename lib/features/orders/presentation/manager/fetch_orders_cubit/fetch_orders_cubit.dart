f63059add8a5529990a40586921eec1e66bc4d4c
Abdulrahman Dawoud <kabdo2223@gmail.com>
2025-11-13 21:03:25 +0200

    manage streams

--- lib/features/orders/presentation/manager/fetch_orders_cubit/fetch_orders_cubit.dart
+++ lib/features/orders/presentation/manager/fetch_orders_cubit/fetch_orders_cubit.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:e_commerce_dashboard/features/orders/domain/entities/data/models/order_entity.dart';
 import 'package:e_commerce_dashboard/features/orders/domain/repos/order_repo.dart';
@@ -8,14 +10,19 @@ part 'fetch_orders_state.dart';
 class FetchOrdersCubit extends Cubit<FetchOrdersState> {
   FetchOrdersCubit(this.orderRepo) : super(FetchOrdersInitial());
   final OrderRepo orderRepo;
-
-  void fetchOrders() async {
+  StreamSubscription? _streamSubscription;
+  void fetchOrders() {
     emit(FetchOrdersLoading());
-    await for (var result in orderRepo.fetchOrders()) {
+    _streamSubscription = orderRepo.fetchOrders().listen((result) {
       result.fold(
         (failure) => emit(FetchOrdersFailure(errorMessage: failure.message)),
         (orders) => emit(FetchOrdersSuccess(orders: orders)),
       );
+    });
+    @override
+    Future<void> close() {
+      _streamSubscription?.cancel();
+      return super.close();
     }
   }
 }
